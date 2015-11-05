4a82dc1a44888c48235adeef2a8524d567c6feb1
Trodis <ferhat.oezmen@hs-bochum.de>
2015-11-05 03:46:11 +0100

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,10 @@
 from app.Application import Application
 
-CONSUMER_KEY        = "oFnKOZ1a4BJMOMjCkJbb7rv2i"
-CONSUMER_SECRET     = "8V6V7w26vy0kUl99vNmZg3Fod8RLl1nLuxslDhh0T0BwhxN6mD"
-REQUEST_TOKEN_URL   = "https://api.twitter.com/oauth/request_token"
-ACCESS_TOKEN_URL    = "https://api.twitter.com/oauth/access_token"
-AUTHORIZE_URL       = "https://api.twitter.com/oauth/authorize"
+CONSUMER_KEY        =
+CONSUMER_SECRET     =
+REQUEST_TOKEN_URL   =
+ACCESS_TOKEN_URL    =
+AUTHORIZE_URL       =
 
 if __name__ == "__main__":
     app = Application(CONSUMER_KEY,CONSUMER_SECRET, 
